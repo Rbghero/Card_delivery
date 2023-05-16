ee8c50b704789d9c78cae9520e1c094723aafc44
Oleg <rbghero@gmail.com>
2023-05-16 13:25:10 +0300

    Fixed 5 days for 3 days

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -20,7 +20,7 @@ public class CardDeliveryTest {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Краснодар");
-        String currentDate = generateDate(5, "dd.MM.yyyy");
+        String currentDate = generateDate(3, "dd.MM.yyyy");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
         $("[data-test-id='date'] input").sendKeys(currentDate);
         $("[data-test-id='name'] input").setValue("Иванов Иван");
